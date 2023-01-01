2c9bd738db7efe55266682ffbe40fcf62442da8d
Reda El Bettioui <redaelbettioui@gmail.com>
2023-01-01 07:05:37 -0800

    new test

---
+++ app/src/main/java/com/pet/adoption/test.java
@@ -0,0 +1,13 @@
+package com.pet.adoption;
+
+public class test {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
